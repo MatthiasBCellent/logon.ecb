35ff28435e088d691895f1a5d54a35d60375cb10
Matthias Bohnen <mbohnen@brockhaus-gruppe.de>
2015-10-30 08:05:14 +0100

    dao injected into control layer

--- personService_impl/src/main/java/de/cellent/personService/control/PersonServiceBean.java
+++ personService_impl/src/main/java/de/cellent/personService/control/PersonServiceBean.java
@@ -1,5 +1,6 @@
 package de.cellent.personService.control;
 
+import javax.ejb.EJB;
 import javax.ejb.Local;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
@@ -7,11 +8,19 @@ import javax.ejb.Stateless;
 import de.cellent.ecb.util.control.GenericServiceBean;
 import de.cellent.personService.boundary.rmi.PersonService;
 import de.cellent.personService.boundary.rmi.PersonServiceLocal;
+import de.cellent.personService.entity.PersonDAO;
 
 @Stateless
 @Remote(PersonService.class)
 @Local(PersonServiceLocal.class)
 public class PersonServiceBean extends GenericServiceBean implements PersonService, PersonServiceLocal {
 
-	// not much to do, all is done by GenericServiceBean
+	// not used in here, just to show how to make use of it
+	@EJB
+	private PersonDAO dao;
+	
+	// not much to do, all is done by GenericServiceBean 
+	// (but specific code might go here)
+	
+	
 }
